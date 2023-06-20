a2a5ae8468d8ba5ff04b3c87eab9793f864d91a9
jayesh thakare <56386987+jayeshthk@users.noreply.github.com>
2023-06-21 00:30:37 +0530

    Update lora.py

--- finetune/lora.py
+++ finetune/lora.py
@@ -33,7 +33,7 @@ devices = 1
 # Hyperparameters
 learning_rate = 3e-4
 batch_size = 128
-micro_batch_size = 4
+micro_batch_size = 1
 gradient_accumulation_iters = batch_size // micro_batch_size
 assert gradient_accumulation_iters > 0
 max_iters = 50000  # train dataset size
